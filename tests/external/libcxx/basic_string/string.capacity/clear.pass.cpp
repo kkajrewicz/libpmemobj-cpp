9ce3036c3ab10bebd94762607ac7297b9604b291
Krzysztof Kajrewicz <krzysztof.kajrewicz@intel.com>
2019-04-15 13:33:47 +0200

    tests: capacity methods

--- tests/external/libcxx/basic_string/string.capacity/clear.pass.cpp
+++ tests/external/libcxx/basic_string/string.capacity/clear.pass.cpp
@@ -21,59 +21,59 @@ namespace pmem_exp = pmem::obj::experimental;
 using S = pmem_exp::string;
 
 struct root {
-  nvobj::persistent_ptr<S> s;
+	nvobj::persistent_ptr<S> s;
 };
 
 template <class S>
 void
 test(nvobj::pool<struct root> &pop, S &s)
 {
-    s.clear();
-    UT_ASSERT(s.size() == 0);
+	s.clear();
+	UT_ASSERT(s.size() == 0);
 }
 
 int
 main(int argc, char *argv[])
 {
-  START();
+	START();
 
-  if (argc < 2) {
-	std::cerr << "usage: " << argv[0] << " file-name" << std::endl;
-	return 1;
-  }
-
-  auto path = argv[1];
-  auto pop = nvobj::pool<root>::create(
-	  path, "string_test", PMEMOBJ_MIN_POOL, S_IWUSR | S_IRUSR);
-
-  auto r = pop.root();
-  {
-	try {
-	  nvobj::transaction::run(pop, [&] {
-		r->s = nvobj::make_persistent<S>();
-	  });
-
-	  auto &s = *r->s;
-
-	  test(pop, s);
-
-	  s.assign(10, 'a');
-	  s.erase(5);
-	  test(pop, s);
-
-	  s.assign(100, 'a');
-	  s.erase(50);
-	  test(pop, s);
-
-	  nvobj::transaction::run(pop, [&] {
-		  nvobj::delete_persistent<S>(r->s);
-	  });
-	} catch (std::exception &e) {
-	  UT_FATALexc(e);
+	if (argc < 2) {
+		std::cerr << "usage: " << argv[0] << " file-name" << std::endl;
+		return 1;
 	}
-  }
 
-  pop.close();
+	auto path = argv[1];
+	auto pop = nvobj::pool<root>::create(
+		path, "string_test", PMEMOBJ_MIN_POOL, S_IWUSR | S_IRUSR);
 
-  return 0;
+	auto r = pop.root();
+	{
+		try {
+			nvobj::transaction::run(pop, [&] {
+				r->s = nvobj::make_persistent<S>();
+			});
+
+			auto &s = *r->s;
+
+			test(pop, s);
+
+			s.assign(10, 'a');
+			s.erase(5);
+			test(pop, s);
+
+			s.assign(100, 'a');
+			s.erase(50);
+			test(pop, s);
+
+			nvobj::transaction::run(pop, [&] {
+				nvobj::delete_persistent<S>(r->s);
+			});
+		} catch (std::exception &e) {
+			UT_FATALexc(e);
+		}
+	}
+
+	pop.close();
+
+	return 0;
 }
